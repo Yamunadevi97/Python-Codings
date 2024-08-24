a02e572d5ae978fa07ded16d154f7e61f70ceb54
A.Yamunadevi <yamunadeviannamalai@gmail.com>
2024-08-24 19:09:44 +0530

    Create Band_Name.py

---
+++ Band_Name.py
@@ -0,0 +1,15 @@
+#Band name generator
+print("Band Name Generator")
+a = input("what's the name of the  city you grew up in ? ")
+print(a)
+b = input("What's your pet's name? ")
+print(b)
+print("your band name could be? ",a,b)
+
+#OUTPUT
+Band Name Generator
+what's the name of the  city you grew up in ? Bristol
+Bristol
+What's your pet's name? Rabbit
+Rabbit
+your band name could be?  Bristol Rabbit
